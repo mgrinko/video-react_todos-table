d7d9504646e911f986801f957f244eb40302fbfa
mhrynko <glukym@gmail.com>
2022-08-23 18:39:38 +0300

    after NavLinks

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { todos } from './data/todos';
 import { TodoTable } from './components/TodoTable';
-import { Link, Navigate, Outlet, Route, Routes, useParams } from 'react-router-dom';
+import { Link, Navigate, NavLink, Route, Routes, useParams } from 'react-router-dom';
+import classNames from 'classnames';
 
 export const TodosPage = () => {
   const { todoId = 0 } = useParams();
@@ -23,13 +24,35 @@ export const App = () => {
           <img src="/logo.svg" alt="MA" className="logo" />
         </Link>
 
-        <Link to="/" className="navbar-item">Home</Link>
-        <Link to="todos" className="navbar-item">Todos</Link>
+        <NavLink
+          to="/"
+          className={({ isActive }) => classNames('navbar-item', { 'is-active': isActive })}
+          style={({ isActive }) => ({ color: isActive ? 'red' : 'black' })}
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="todos"
+          className={({ isActive }) => classNames('navbar-item', { 'is-active': isActive })}
+          style={({ isActive }) => ({ color: isActive ? 'red' : 'black' })}
+        >
+          Todos
+        </NavLink>
       </div>
     </nav>
 
     <div className="section">
-      <Outlet />
+      <Routes>
+        <Route path="todos">
+          <Route index element={<TodosPage />} />
+          <Route path=":todoId" element={<TodosPage />} />
+        </Route>
+
+        <Route path="/" element={<h1 className="title">Home Page</h1>} />
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<p>Page not found</p>} />
+      </Routes>
     </div>
   </>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,23 +2,12 @@ import ReactDOM from 'react-dom/client';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
-import { App, TodosPage } from './App';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const Root = () => (
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="todos">
-          <Route index element={<TodosPage />} />
-          <Route path=":todoId" element={<TodosPage />} />
-        </Route>
-
-        <Route index element={<h1 className="title">Home Page</h1>} />
-        <Route path="home" element={<Navigate to="/" replace />} />
-        <Route path="*" element={<p>Page not found</p>} />
-      </Route>
-    </Routes>
+    <App />
   </BrowserRouter>
 );
 
